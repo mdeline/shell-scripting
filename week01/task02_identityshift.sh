f880bc76025bb31c721188ae0baa9fedd4c34356
Ella Berg <ellamadelineberg@gmail.com>
2020-11-08 18:41:35 +0200

    Finish task 2

---
+++ week01/task02_identityshift.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+# Add alias for ls
+# Add alias for opening man pages in the browser (name = cman)
+
+alias list="ls"
+alias list_all="ls -a"
+alias cman="man --html=google-chrome" # Käyttö esim. cman grep. Tää ei muuten toimi Mac:ssa
