316acfbda9a4026b0c77257e7826e9483c74d7a3
Ella Berg <ellamadelineberg@gmail.com>
2020-11-14 21:05:17 +0200

    Finish task 9

--- week02/task09_pipelines.sh
+++ week02/task09_pipelines.sh
@@ -1,1 +1,10 @@
 #! /bin/bash
+# Task 09: Connect one program's stout to another's stdin
+
+# a. Redirect the output of ls to a file and then count the number of lines in the file
+ls | tee files.txt | wc -l # tee writes the output both to the screen (stdout) and to the file (stdin)
+
+# b. Modify the command to count only the number of folders in your home directory
+ls -d ~/ | tee folders.txt | wc -l
+# or
+ls -l ~/ | grep '^d' | tee folders.txt | wc -l
