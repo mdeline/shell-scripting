4eb0eb84c83144f207b3f586b7e84a16ad7c1769
Ella Berg <ellamadelineberg@gmail.com>
2020-11-15 19:08:27 +0200

    Fix task 6

--- week01/task06_insertdate.sh
+++ week01/task06_insertdate.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-# Create a script rsync-todays-Exactum-cam.sh that uses echo to print a rsync
-# command with the correct paths for the current date. Paste the code below.
-echo "rsync --archive --stats https://www.cs.helsinki.fi/u/tkt_cam/$(date +%Y/%m/%d) ~/ShellScripting2019/Week1/$(date +"%A.%Y.%m.%d")"
+# Create a script rsync-todays-Exactum-cam.sh that uses echo to print a rsync command with the correct paths for the current date. Paste the code below.
+echo "rsync --archive --stats /cs/home/tkt_cam/public_html/$(date +%Y/%m/%d) ~/ShellScripting2019/Week1/$(date +"%A.%Y.%m.%d")"
