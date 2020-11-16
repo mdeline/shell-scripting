a4d827463a7178cca232ffbfc1103f5e13f7e0bb
Ella Berg <ellamadelineberg@gmail.com>
2020-11-16 23:11:05 +0200

    Finish task 16

--- week02/task16_ls_bb.sh
+++ week02/task16_ls_bb.sh
@@ -1,1 +1,5 @@
 #! /bin/bash
+
+# Write a script that finds the image files corresponding to the current month and counts them. Use find, date, and wc -l
+CURRENT_MONTH=$(date +%Y/%m/)
+find /cs/home/tkt_cam/public_html/$CURRENT_MONTH -type f -name '*.jpg' | wc -l
