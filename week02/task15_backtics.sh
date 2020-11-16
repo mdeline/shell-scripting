ebdda696d32825063de5e37f27649fd119de4b7b
Ella Berg <ellamadelineberg@gmail.com>
2020-11-16 23:10:46 +0200

    Finish task 15

--- week02/task15_backtics.sh
+++ week02/task15_backtics.sh
@@ -1,1 +1,6 @@
 #! /bin/bash
+
+# Combine backticks (`) and the date command from the exercises of Week 1 to list
+# the number of image files of the current month. In this case, it's now November.
+CURRENT_MONTH=$(date +%Y/%m/)
+ls /cs/home/tkt_cam/public_html/$CURRENT_MONTH | grep -i jpg$ | wc -l
