26e84585db9ae4d144d5b009e589924541ddff66
Ella Berg <ellamadelineberg@gmail.com>
2020-11-16 23:09:42 +0200

    Fix task 13

--- week02/task13_pictures.sh
+++ week02/task13_pictures.sh
@@ -2,4 +2,4 @@
 
 # Write another script that skips the subdirectories from the previous exercise.
 # In other words, it only lists the picture files ending in .jpg
-ls /cs/home/tkt_cam/public_html/2011/11/ | grep $pattern *.jpg
+ls /cs/home/tkt_cam/public_html/2011/11/ | grep -i jpg$
